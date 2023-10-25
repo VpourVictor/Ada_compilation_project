5396e1b861a709784c2b4ad0751551adea9ee827
Lili <lili.schmidlin@telecomnancy.eu>
2023-10-25 15:33:48 +0200

    mise en place environnement et début code

--- lexeur/file_to_code.py
+++ lexeur/file_to_code.py
@@ -1,17 +1,39 @@
 # import du dictionnaire
 
-# lecture du fichier à reconnaitre
-# transformation des caractères du fichier en liste de token
-# par rapport au dictionnaire
-# on retourne une liste de token
+def get_token(name_file):
+    # ouverture du fichier
+    file = open(name_file, "r")
+    # initialisation de la liste de token
+    token_list = []
 
-# si la chaîne est un identifiant (ex : if) on le remplacera directement par le code associé
-# si caractère de commentaire, on supprime ce qui est avant la balise de commentaire /* ... */
+    # lecture du fichier caractère par caractère
+    for ligne in file:
+        for char in ligne:
+            print(char)
+            # si le caractère est un espace, on passe au caractère suivant
+            if char == " ":
+                continue
+            # si le caractère est un retour à la ligne, on passe au caractère suivant
+            elif char == "\n":
+                continue
+            # si le caractère est un tab, on passe au caractère suivant
+            elif char == "\t":
+                continue
 
-# exemple : with = 284
-# A = 65
-# I = 79
+    # transformation des caractères du fichier en liste de token
+    # par rapport au dictionnaire
+    # on retourne une liste de token
 
-# si pas d'espace
-# on lit tout, si le total n'est pas dans le dictionnaire,
-# on reconnait : séparément les caractères
+    # si la chaîne est un identifiant (ex : if) on le remplacera directement par le code associé
+    # si caractère de commentaire, on supprime ce qui est avant la balise de commentaire /* ... */
+
+    # exemple : with = 284
+    # A = 65
+    # I = 79
+
+    # si pas d'espace
+    # on lit tout, si le total n'est pas dans le dictionnaire,
+    # on reconnait : séparément les caractères
+
+    # fermeture du fichier
+    file.close()
---
+++ lexeur/main_lexeur.py
@@ -0,0 +1,4 @@
+from file_to_code import get_token
+
+if __name__ == '__main__':
+    get_token("exemple.ada")
