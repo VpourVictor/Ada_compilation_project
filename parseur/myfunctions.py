57f71209d591c82f455db3785f0cf074b76b5599
LEVREL Victor <victor.levrel@telecomnancy.eu>
2023-12-12 13:01:00 +0100

    modele parseur

--- parseur/myfunctions.py
+++ parseur/myfunctions.py
@@ -1,45 +1,64 @@
 from lexeur.file_to_code_ter import get_token
 
-
+count_tmp = 0 #
 count = 0
 list_token = get_token("name_file")
 
-def fonction_N1(list_token): #N1 :284 285 A1 46 
+def fonction_ (list_token):
     global count
-    error = False
-    if reader_carac(284, list_token[count],count) :
-       
-        if reader_carac(285, list_token[count], count) :
-            if fonction_A1(list_token)[0] :
-                count += fonction_A1[1]
-                if reader_carac(285, list_token[count], count) :
-                    return (True, count)
-                else :
-                    return (False, count)
-            else :
-                count += fonction_A1[1]
-                return (False, count)
-        else :
-            return(False, count)
-    else : 
-        return(False, count)
+    global count_tmp
+
+def fonction_N16(): #N16 ::= 34 A16 34
+    global count
+    global count_tmp
+    local_ind = 0
+    if reader_carac(34) :
+        if fonction_A16():
+            if reader_carac(34) :
+    return False
+
+def fonction_A16(): #A16 ::= N12 | N13 | 44 | 58 | 40 | 41 | 45 | 61 | 60 | 62 | 43 | 42 | 47 | 46 | 95 | 34
+    global count
+    global count_tmp
+    if reader_carac(34) :
+        return True
+    elif reader_carac()
+
+
+
 
 
     
 
-def reader_carac(p1, p2, count):
-    if p1 == p2:  # 284 = with
-        count += 1
+def reader_carac(p1):
+    #traiter le cas ou p2 est un couple
+    global count
+    global count_tmp
+    if p1 == token_list[count_tmp]:  # 284 = with
+        count_tmp += 1
         return True
-    elif compare([0][1], "with") < 2 :
+    elif compare(token_list[count_tmp][1], 284) :
        print("did you mean with, reçu : " + find_receive(list_token[count]))
-       count += 1
+       count_tmp += 1
        return True
-    elif forgot_first_letter([0][1], "with")  :
+    elif forgot_first_letter(p1, "with")  :
        print("you forgot first letter " + find_receive(list_token[count]))
-       count += 1
+       count_tmp += 1
        return True
     else :
         print("attendu : with, reçu : " + find_receive(list_token[count]))
         return False
     
+def test_is_correct(p1, str) :
+
+    #relier les cas au dictionnaire
+
+    def compare(p,str):
+
+        return False
+    
+    def forgot_a_letter(p,str):
+
+        return False
+    
+    return (True)
