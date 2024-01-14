94062a21d0f69b2ef9b3633460176a5294e15964
Lili <lili.schmidlin@telecomnancy.eu>
2024-01-14 17:34:31 +0100

    set up tests arbres

---
+++ parseur/tests/test_arbre.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+    def test_arbre(self):
+        # TODO test génération arbres
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
