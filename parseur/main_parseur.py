878de142ccf1d34fe981928b0e191f8b0bcce9ad
LEVREL Victor <victor.levrel@telecomnancy.eu>
2024-01-16 20:14:54 +0100

    AST en cours

--- parseur/main_parseur.py
+++ parseur/main_parseur.py
@@ -4,6 +4,10 @@ from anytree.exporter import DotExporter
 import lexeur.file_to_code_ter as file
 import parseur.functions
 
+# def delete_EXPR1(root):
+#     for pre, _, node in RenderTree(root):
+#         if "N9" in node.name :
+
 
 def generate_tree(name_file):
     try:
@@ -28,29 +32,46 @@ def isDelete(name):
     nom = name.split(" ")
     return nom[1][0].isalpha()
 
+def supprimer_noeuds_un_seul_fils(node):
+    children = node.children
+    if len(children) == 1 :
+        #if len(children) == 1 and not children[0].is_leaf:
+        # Si le nœud a un seul fils qui n'est pas une feuille, le supprimer
+        parent = node.parent
+        node.parent = None  # Cela supprime le nœud de son parent
+        children[0].parent = parent
+        return True
+    return False
+
+def delete_all_nodes(root):
+    for node in PreOrderIter(root):
+        if supprimer_noeuds_un_seul_fils(node):
+            delete_all_nodes(root)
+
 
 if __name__ == '__main__':
-    print("On va maintenant tester notre parseur")
-    print("Pour chaque test, on va afficher la liste de token renvoyé par le lexeur, puis générer l'arbre syntaxique "
-          "associé")
-    print("On va tester les exemples suivants :")
-    print("On commence par un exemple simple : un programme qui se charge de faire une somme entre 2 entiers :")
-    token_list = file.get_token("exemples/exemple_calcul.ada")
-    print(token_list)
-    root = Node('N1')
-    print(parseur.functions.fonction_N1(token_list, root))
-    delete_leef_epsX(root)
-    generate_tree("tree_calcul.png")
-    result = search.findall(root, filter_=lambda node: node.name in ("33 285"))
-    print(result)
-
-    # print("On va maintenant tester un exemple qui contient des procédures imbriquées :")
-    # token_list = file.get_token("exemples/exemple_double_procedure.ada")
+    # print("On va maintenant tester notre parseur")
+    # print("Pour chaque test, on va afficher la liste de token renvoyé par le lexeur, puis générer l'arbre syntaxique "
+    #       "associé")
+    # print("On va tester les exemples suivants :")
+    # print("On commence par un exemple simple : un programme qui se charge de faire une somme entre 2 entiers :")
+    # token_list = file.get_token("exemples/exemple_calcul.ada")
     # print(token_list)
     # root = Node('N1')
     # print(parseur.functions.fonction_N1(token_list, root))
     # delete_leef_epsX(root)
-    # generate_tree("tree_double_procedure.png")
+    # generate_tree("tree_calcul.png")
+    # result = search.findall(root, filter_=lambda node: node.name in ("33 285"))
+    # print(result)
+
+    print("On va maintenant tester un exemple qui contient des procédures imbriquées :")
+    token_list = file.get_token("exemples/exemple_double_procedure.ada")
+    print(token_list)
+    root = Node('N1')
+    print(parseur.functions.fonction_N1(token_list, root))
+    delete_leef_epsX(root)
+    delete_all_nodes(root)
+    generate_tree("tree_double_procedure.png")
     #
     # print("On va maintenant tester un exemple qui contient blocs d'instructions if elif et else :")
     # token_list = file.get_token("exemples/exemple_if_elif.ada")
@@ -83,7 +104,7 @@ if __name__ == '__main__':
     # print(parseur.functions.fonction_N1(token_list, root))
     # delete_leef_epsX(root)
     # generate_tree("tree_expression_arithmetique.png")
-    #
+
     # print("On va maintenant tester un exemple qui contient des fonctions imbriquées :")
     # token_list = file.get_token("exemples/exemple_fonctions_imb.ada")
     # print(token_list)
